7d7132ec51a55c290bcc7f3ea3e4ecfa4f62790b
coolyoshi <yoonchris@live.ca>
2015-03-23 14:55:40 -0700

    oops

--- src/ca/ubc/cs/cs304/steemproject/base/released/CreditCard.java
+++ src/ca/ubc/cs/cs304/steemproject/base/released/CreditCard.java
@@ -12,10 +12,10 @@ public class CreditCard {
         if (aCardOwner == null) {
             throw new IllegalArgumentException("Card owner cannot be null.");
         }
-        if (aCardNumber.matches("[0-9]+") == false && aCardNumber.length() != 16) {
+        if (aCardNumber.matches("[0-9]+") == false || aCardNumber.length() != 16) {
             throw new IllegalArgumentException("Card number must be a 16 digit number.");
         }
-        if (aCcv.matches("[0-9]+") == false && aCcv.length() != 3) {
+        if (aCcv.matches("[0-9]+") == false || aCcv.length() != 3) {
             throw new IllegalArgumentException("CCV must be a 3 digit number.");
         }
         if (aAddress == null) {
