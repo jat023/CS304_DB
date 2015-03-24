94e7607a2124137b1e5e0d15f6822703d04f58f1
tqichen <rickychen92@gmail.com>
2015-03-24 14:52:47 -0700

    Partially complete

--- src/ca/ubc/cs/cs304/steemproject/ui/panels/GameTesterPanel.java
+++ src/ca/ubc/cs/cs304/steemproject/ui/panels/GameTesterPanel.java
@@ -1,7 +1,85 @@
 package ca.ubc.cs.cs304.steemproject.ui.panels;
 
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+import ca.ubc.cs.cs304.steemproject.access.Accessors;
+import ca.ubc.cs.cs304.steemproject.access.IGameTesterAccessor;
+import ca.ubc.cs.cs304.steemproject.access.options.GameSortByOption;
+import ca.ubc.cs.cs304.steemproject.access.options.SortDirection;
+import ca.ubc.cs.cs304.steemproject.base.Genre;
+import ca.ubc.cs.cs304.steemproject.base.development.GameTester;
+
+@SuppressWarnings("serial")
 public class GameTesterPanel extends JPanel {
+    
+    private final IGameTesterAccessor fGameTesterAccessor;
+    private final GameTester fGameTester;
+    
+    private final JTextField fGameNameField;
+    private final JTextField fGameDeveloperField;
+    private final JComboBox<Genre> fGameGenreField;
+    private final JComboBox<GameSortByOption> fGameSortOptionField;
+    private final JComboBox<SortDirection> fGameSortDirectionField;
+    
+    public GameTesterPanel(IGameTesterAccessor aGameTesterAccessor, GameTester aGameTester) {
+        
+        if (aGameTesterAccessor == null) {
+            throw new IllegalArgumentException("Game tester accessor cannot be null.");
+        }
+        if (aGameTester == null) {
+            throw new IllegalArgumentException("Game tester cannot be null.");
+        }
+        fGameTesterAccessor = aGameTesterAccessor;
+        fGameTester = aGameTester;
+        
+        setLayout(null);
+        
+        JLabel nameLabel = new JLabel("Game Title");
+        nameLabel.setBounds(10, 10, 80, 25);
+        this.add(nameLabel);
+
+        fGameNameField = new JTextField("", 15);
+        fGameNameField.setBounds(100, 10, 190, 25);
+        this.add(fGameNameField);
+
+        JLabel developerLabel = new JLabel("Developer");
+        developerLabel.setBounds(10, 40, 80, 25);
+        this.add(developerLabel);
+
+        fGameDeveloperField = new JTextField("", 20);
+        fGameDeveloperField.setBounds(100, 40, 190, 25);
+        this.add(fGameDeveloperField);
+     
+        JLabel genreLabel = new JLabel("Genre");
+        genreLabel.setBounds(10, 70, 80, 25);
+        this.add(genreLabel);
+
+        fGameGenreField = new JComboBox<Genre>(Genre.values());
+        fGameGenreField.setBounds(100, 70, 190, 25);
+        this.add(fGameGenreField);
+        
+        JLabel sortByLabel = new JLabel("Sort By");
+        sortByLabel.setBounds(10, 100, 80, 25);
+        this.add(sortByLabel);
+
+        fGameSortOptionField = new JComboBox<GameSortByOption>(GameSortByOption.values());
+        fGameSortOptionField.setBounds(100, 100, 100, 25);
+        this.add(fGameSortOptionField);
+
+        fGameSortDirectionField = new JComboBox<SortDirection>(SortDirection.values());
+        fGameSortDirectionField.setBounds(210, 100, 80, 25);
+        this.add(fGameSortDirectionField);
+    }
+    
+    public static final void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.add(new GameTesterPanel(Accessors.getGameTesterAccessor(),  new GameTester(1, "gametester1@gmail.com", "Pass1")));
+        frame.setSize(500,500);
+        frame.setVisible(true);
+    }
 
 }
