44b3e31f98ae1de040612a0ded0dca792fcfaef7
tqichen <rickychen92@gmail.com>
2015-03-24 14:30:31 -0700

    Completed login panel.

--- src/ca/ubc/cs/cs304/steemproject/ui/panels/LoginPanel.java
+++ src/ca/ubc/cs/cs304/steemproject/ui/panels/LoginPanel.java
@@ -1,7 +1,152 @@
 package ca.ubc.cs.cs304.steemproject.ui.panels;
 
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+
+import ca.ubc.cs.cs304.steemproject.access.ILoginAccessor;
+import ca.ubc.cs.cs304.steemproject.base.development.GameTester;
+import ca.ubc.cs.cs304.steemproject.base.released.Customer;
+import ca.ubc.cs.cs304.steemproject.exception.PasswordIncorrectException;
+import ca.ubc.cs.cs304.steemproject.exception.UserNotExistsException;
+
+@SuppressWarnings("serial")
 public class LoginPanel extends JPanel {
 
+    private final ILoginAccessor fLoginAccessor;
+    private final JComboBox<UserType> fTypeField;
+    private final JTextField fEmailField;
+    private final JPasswordField fPasswordField;
+
+    private LoginStatus loginStatus;
+    private Customer loggedInCustomer;
+    private GameTester loggedInGameTester;
+
+    public LoginPanel(ILoginAccessor aLoginAccessor) {
+
+        if (aLoginAccessor == null) {
+            throw new IllegalArgumentException("Login accessor cannot be null.");
+        }
+        fLoginAccessor = aLoginAccessor;
+
+        loginStatus = LoginStatus.NOTLOGGEDIN;
+
+        this.setLayout(null);
+
+        JLabel userLabel = new JLabel("User");
+        userLabel.setBounds(10, 10, 80, 25);
+        this.add(userLabel);
+
+        fEmailField = new JTextField("", 20);
+        fEmailField.setBounds(100, 10, 160, 25);
+        this.add(fEmailField);
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 40, 80, 25);
+        this.add(passwordLabel);
+
+        fPasswordField = new JPasswordField("", 20);
+        fPasswordField.setBounds(100, 40, 160, 25);
+        this.add(fPasswordField);
+
+        JButton loginButton = new JButton("LOGIN");
+        loginButton.setBounds(10, 80, 80, 25);
+        loginButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                String email = fEmailField.getText();
+                String password = String.valueOf(fPasswordField.getPassword());
+                UserType type = (UserType) fTypeField.getSelectedItem();
+
+                switch (type) {
+                case REGULAR_USER:
+                    try {
+                        fLoginAccessor.loginCustomer(email, password);
+                        loggedInCustomer = fLoginAccessor.lookupCustomer(email);
+                        loginStatus = LoginStatus.CUSTOMER;
+                        JOptionPane.showMessageDialog(null, "Welcome " + email, "LOGIN SUCCESS", JOptionPane.INFORMATION_MESSAGE);
+                    } catch (UserNotExistsException e) {
+                        JOptionPane.showMessageDialog(null, "No user with given this email exists.", "LOGIN FAILED", JOptionPane.INFORMATION_MESSAGE);
+                    } catch (PasswordIncorrectException e) {
+                        JOptionPane.showMessageDialog(null, "Incorrect password.", "LOGIN FAILED", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                    break;
+                case GAME_TESTER:
+                    try {
+                        fLoginAccessor.loginGametester(email, password);
+                        loggedInCustomer = fLoginAccessor.lookupCustomer(email);
+                        loginStatus = LoginStatus.GAMETESTER;
+                        JOptionPane.showMessageDialog(null, "Welcome " + email, "LOGIN SUCCESS", JOptionPane.INFORMATION_MESSAGE);
+                    } catch (UserNotExistsException e) {
+                        JOptionPane.showMessageDialog(null, "No game tester with this email exists.", "LOGIN FAILED", JOptionPane.INFORMATION_MESSAGE);
+                    } catch (PasswordIncorrectException e) {
+                        JOptionPane.showMessageDialog(null, "Incorrect password.", "LOGIN FAILED", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                    break;
+                default:
+                    throw new IllegalStateException("User type is " +type);
+                }
+                
+                fEmailField.setText("");
+                fPasswordField.setText("");
+            }
+        });
+
+        this.add(loginButton);
+
+        fTypeField = new JComboBox<UserType>(UserType.values());
+        fTypeField.setBounds(100, 80, 160, 25);
+        this.add(fTypeField);
+
+        this.setSize(270, 120);
+        this.setBorder(new LineBorder(Color.ORANGE));
+    }
+
+    /**
+     * resets the login status.
+     */
+    public void logout() {
+        loginStatus = LoginStatus.NOTLOGGEDIN;
+    }
+
+    public LoginStatus getLoginStatus() {
+        return loginStatus;
+    }
+
+    public Customer getCustomer() {
+        if (loginStatus.equals(LoginStatus.CUSTOMER)) {
+            return loggedInCustomer;
+        } else {
+            throw new IllegalStateException("No customer has logged in.");
+        }
+    }
+
+    public GameTester getGameTester() {
+        if (loginStatus.equals(LoginStatus.GAMETESTER)) {
+            return loggedInGameTester;
+        } else {
+            throw new IllegalStateException("No game tester has logged in.");
+        }
+    }
+
+    public enum LoginStatus {
+        NOTLOGGEDIN,
+        CUSTOMER,
+        GAMETESTER;
+    }
+
+    private enum UserType {
+        REGULAR_USER,
+        GAME_TESTER;
+    }
 }
